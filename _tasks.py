1d5fbae36a42481384247fdfdf121c467b0dbd7c
Robert Richer <robert.richer@fau.de>
2025-05-19 11:39:04 +0200

    after tooling

--- _tasks.py
+++ _tasks.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 import sys
 from pathlib import Path
+from typing import Sequence
 
 HERE = Path(__file__).parent
 
@@ -22,7 +23,7 @@ def update_version_strings(file_path, new_version):
     # taken from:
     # https://stackoverflow.com/questions/57108712/replace-updated-version-strings-in-files-via-python
     version_regex = re.compile(r"(^_*?version_*?\s*=\s*\")(\d+\.\d+\.\d+-?\S*)\"", re.M)
-    with open(file_path, "r+") as f:
+    with file_path.open("r+") as f:
         content = f.read()
         f.seek(0)
         f.write(
@@ -35,16 +36,23 @@ def update_version_strings(file_path, new_version):
         f.truncate()
 
 
-def update_version(version):
-    subprocess.run(["poetry", "version", version], shell=False, check=True)
-    new_version = (
-        subprocess.run(["poetry", "version"], shell=False, check=True, capture_output=True)
-        .stdout.decode()
-        .strip()
-        .split(" ", 1)[1]
-    )
-    update_version_strings(HERE.joinpath("pepbench_annotation/__init__.py"), new_version)
+def update_version(version: Sequence[str]):
+    if len(version) == 0:
+        # no argument passed => return the current version
+        subprocess.run(["uv", "version"], shell=False, check=True, capture_output=False)
+    else:
+        # update the version
+        subprocess.run(["uv", "version", *version], shell=False, check=True)
+        new_version = (
+            subprocess.run(["uv", "version"], shell=False, check=True, capture_output=True)
+            .stdout.decode()
+            .strip()
+            .split(" ", 1)[1:][0]
+        )
+
+        update_version_strings(HERE.joinpath("src/pepbench_annotation/__init__.py"), new_version)
 
 
 def task_update_version():
-    update_version(sys.argv[1])
+    version_arr = sys.argv[1:] if len(sys.argv) > 1 else []
+    update_version(version_arr)
